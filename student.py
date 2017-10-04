4c708e3df504a548535769f9e9770924015285a2
leona0409 <leona0409@gmail.com>
2017-10-04 14:37:02 -0400

    New dance

--- student.py
+++ student.py
@@ -72,7 +72,7 @@ class Piggy(pigo.Pigo):
 
     def safety_check(self):
         self.servo(self.MIDPOINT)  # look straight ahead
-        for x in range(4):
+        for x in range(8):
             if not self.is_clear():
                 print("NOT GOING TO DANCE")
                 return False
