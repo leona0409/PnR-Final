8786f3552579b0be224b238c824b1b1c5409487f
leona0409 <leona0409@gmail.com>
2017-10-13 14:37:55 -0400

    editing cruise method

--- student.py
+++ student.py
@@ -159,8 +159,8 @@ class Piggy(pigo.Pigo):
             if self.is_clear():
                 self.cruise()
             else:
-                self.encR(10)
-    
+                self.encR(2)
+
 
 
 
