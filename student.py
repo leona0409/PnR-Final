b82f9afb379ac0afc1845f083cd2d5e170a96dae
leona0409 <leona0409@gmail.com>
2017-10-18 14:47:29 -0400

    creating obstacle count method, adding a 360 rotation

--- student.py
+++ student.py
@@ -186,7 +186,7 @@ class Piggy(pigo.Pigo):
                     found_something = False
                     counter += 1
             print("\n-------I see %d object(s)------\n" % counter)
-            self.encR(7)
+            self.encR(6)
 
         #Add in a 360 rotation
         #find the area that has the widest gap and turn robot to that gap
