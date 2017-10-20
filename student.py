8adff42bd337338ab8425b5463acdd14960e39e6
leona0409 <leona0409@gmail.com>
2017-10-20 14:25:05 -0400

    creating obstacle count method, adding a 360 rotation

--- student.py
+++ student.py
@@ -175,7 +175,7 @@ class Piggy(pigo.Pigo):
         for x in range(4):
             counter += self.obstacle_count()
             self.encR(6)
-        print(counter)
+        print("There are",counter, "objects total")
 
     def obstacle_count(self):
         """scans and estimates the number of obstacles within sight"""
