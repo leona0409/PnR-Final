419d833af764887649282cd5d96d84749573a660
leona0409 <leona0409@gmail.com>
2017-10-20 15:11:13 -0400

    adding a method to move the robot to the largest open area

--- student.py
+++ student.py
@@ -41,7 +41,7 @@ class Piggy(pigo.Pigo):
         # You may change the menu if you'd like to add an experimental method
         menu = {"n": ("Navigate forward", self.nav),
                 "d": ("Dance", self.cotton_eye_joe),
-                "o": ("Move to Open Space", self.move_to_open_area),
+                "o": ("Move to Open Area", self.move_to_open_area),
                 "c": ("Calibrate", self.calibrate),
                 "s": ("Check status", self.status),
                 "q": ("Quit", quit_now)
@@ -200,7 +200,6 @@ class Piggy(pigo.Pigo):
         for x in range(65, 115):
             if self.scan[x] > 60:
                 self.fwd(20)
-                self.stop()
 
 
 ####################################################
