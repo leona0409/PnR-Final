23677d31459f709221f0c178df38f3c0c91ad84b
leona0409 <leona0409@gmail.com>
2017-11-20 15:10:23 -0500

    attempting to create a smarter nav method

--- student.py
+++ student.py
@@ -230,7 +230,7 @@ class Piggy(pigo.Pigo):
         right_now = datetime.datetime.utcnow()
         difference = (right_now - self.start_time).seconds
         print("It took you %d seconds to run this" % difference)
-        while self.encR(28):
+        while self.encR():
             if self.scan[x] > 50:
                 time.sleep(2)
                 self.servo(x)
